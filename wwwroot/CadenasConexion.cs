124097460389abe3bafd0780970076e8cbb806b3
mafer1412 <mariafernanda.montanezgarcia.gen23@cbtis105.edu.mx>
2025-03-18 10:12:11 -0600

    Trabajo del martes

---
+++ wwwroot/CadenasConexion.cs
@@ -0,0 +1,4 @@
+public class cadenasconexion {
+    public static string SQL_SERVER = "data source=IPSERVIDOR; initial catalog=BASE DE DATOS; user= NOMBRE DE USUARIO; password=CONTRASEÑA; trustservercertificate=true";
+    public static string Mongo_Db ="mongodb://usuario:password@servidor:puerto";
+}
